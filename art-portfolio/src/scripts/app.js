36bca7bb1428b206f96feb68afb7558e0d38b05a
9jr9000 <albertojrschool@gmail.com>
2025-08-31 22:27:07 -0700

    fix?

--- art-portfolio/src/scripts/app.js
+++ art-portfolio/src/scripts/app.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function loadArtwork() {
         // Sample artwork data
         const artworks = [
-            { title: 'Red Eye', image: '\Users\Jr\GitHub\AnikaArt\art-portfolio\src\scripts\Anika.jpg' },
+            { title: 'Red Eye', image: 'c:\Users\Jr\GitHub\AnikaArt\art-portfolio\src\scripts\Anika.jpg' },
             { title: 'Menu', image: 'path/to/image2.jpg' },
             { title: 'IDK testing', image: 'path/to/image3.jpg' },
             { title: 'Art 4', image: 'path/to/image3.jpg' },
